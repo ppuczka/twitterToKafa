db7abcc3c08a789e2c552d20168d2488a41289b2
ppuczka <p.puczka@gmail.com>
2022-11-02 23:18:44 +0100

    adapt

--- kafka/kafka-admin/src/main/java/com/prpu/microservicesdemo/kafka/admin/config/config/WebClientConfig.java
+++ kafka/kafka-admin/src/main/java/com/prpu/microservicesdemo/kafka/admin/config/config/WebClientConfig.java
@@ -1,10 +1,10 @@
 package com.prpu.microservicesdemo.kafka.admin.config.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 
-@Component
+@Configuration
 public class WebClientConfig {
 
     @Bean
